aec53aed77a4122c4ce9deab116b7c1648aa0dac
TheMozg <fedor.md@gmail.com>
2015-03-21 22:33:23 +0300

    Added missing files

---
+++ Client/NetworkClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Core;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Client
+{
+    class NetworkClient
+    {
+        HttpClient Client = new HttpClient();
+        public NetworkClient(String baseAdress)
+        {
+            Client.BaseAddress = new Uri(baseAdress);
+        }
+        public List<Contact> getAllContacts()
+        {
+            var getTask = Client.GetAsync(Strings.UriGetAll);
+            getTask.Wait();
+            HttpResponseMessage msg = getTask.Result;
+            var parseTask = msg.Content.ReadAsStreamAsync();
+            parseTask.Wait();
+            Stream stream = parseTask.Result;
+            XmlSerializer xmls = new XmlSerializer(typeof(List<Contact>));
+            List<Contact> list = (List<Contact>)xmls.Deserialize(stream);
+            return list;
+        }
+        public List<Contact> searchContacts(String searchString, String searchBy)
+        {
+            MultipartFormDataContent content = new MultipartFormDataContent();
+            content.Add(new StringContent(searchString), Strings.KeySearchString);
+            content.Add(new StringContent(searchBy), Strings.KeySearchType);
+            var searchTask = Client.PostAsync(Strings.UriSearch, content);
+            searchTask.Wait();
+            HttpResponseMessage msg = searchTask.Result;
+            var parseTask = msg.Content.ReadAsStreamAsync();
+            parseTask.Wait();
+            Stream stream = parseTask.Result;
+            XmlSerializer xmls = new XmlSerializer(typeof(List<Contact>));
+            List<Contact> list = (List<Contact>)xmls.Deserialize(stream);
+            return list;
+        }
+        public void postContact(Contact contact)
+        {
+                MultipartFormDataContent content = new MultipartFormDataContent();
+                content.Add(new StringContent(contact.name), Strings.KeyContactName);
+                content.Add(new StringContent(contact.surname), Strings.KeyContactSnam);
+                content.Add(new StringContent(contact.mail), Strings.KeyContactMail);
+                content.Add(new StringContent(contact.number), Strings.KeyContactNumb);
+                var postTask = Client.PostAsync(Strings.UriPostOne, content);
+                postTask.Wait();
+        }
+    }
+}
---
+++ Server/cli.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Core;
+using System.IO;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Server
+{
+    class CLI
+    {
+        AdressBook ab = new AdressBook();
+        public void startActionLoop()
+        {
+            Console.WriteLine("App started");
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/" + Strings.UriGetAll + "/");
+            listener.Prefixes.Add("http://localhost:8080/" + Strings.UriPostOne + "/");
+            listener.Prefixes.Add("http://localhost:8080/" + Strings.UriSearch + "/");
+            listener.Start();
+            Console.WriteLine("Service started");
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Waiting for request");
+                var context = listener.GetContext();
+                Console.WriteLine("Received request");
+                String str = context.Request.RawUrl;
+                if (str.StartsWith("/"))
+                    str = str.Remove(0, 1);
+                Console.WriteLine("Requested uri: {0}", str);
+                if(str == Strings.UriPostOne)
+                {
+                    var parser = new HttpMultipartParser(context.Request.InputStream);
+                    if (parser.Success)
+                    {
+                        Contact contact = new Contact();
+                        contact.name = parser.Parameters[Strings.KeyContactName];
+                        contact.surname = parser.Parameters[Strings.KeyContactSnam];
+                        contact.number = parser.Parameters[Strings.KeyContactNumb];
+                        contact.mail = parser.Parameters[Strings.KeyContactMail];
+                        ab.Add(contact);
+                    }
+                    context.Response.Close();
+                    Console.WriteLine("Processed POST request");
+                }
+                if (str == Strings.UriGetAll)
+                {
+                    var list = ab.MasterList;
+                    XmlSerializer xmls = new XmlSerializer(list.GetType());
+                    var resp = context.Response.OutputStream;
+                    xmls.Serialize(context.Response.OutputStream, list);
+                    resp.Close();
+                    Console.WriteLine("Processed GET request");
+                }
+                if (str == Strings.UriSearch)
+                {
+                    string searchString = "";
+                    string searchBy = "";
+                    var parser = new HttpMultipartParser(context.Request.InputStream);
+                    if (parser.Success)
+                    {
+                        searchString = parser.Parameters[Strings.KeySearchString];
+                        searchBy = parser.Parameters[Strings.KeySearchType];
+                    }
+                    List<Contact> searchResults = new List<Contact>();
+                    switch (searchBy)
+                    {
+                        case Strings.SearchTypeName:
+                            searchResults = ab.Search(searchString, SearchType.Name);
+                            break;
+                        case Strings.SearchTypeNplS:
+                            searchResults = ab.Search(searchString, SearchType.NameAndSurname);
+                            break;
+                        case Strings.SearchTypeNumb:
+                            searchResults = ab.Search(searchString, SearchType.Phone);
+                            break;
+                        case Strings.SearchTypeAll:
+                            searchResults = ab.Search(searchString, SearchType.All);
+                            break;
+                    }
+
+                    XmlSerializer xmls = new XmlSerializer(searchResults.GetType());
+                    var resp = context.Response.OutputStream;
+                    xmls.Serialize(context.Response.OutputStream, searchResults);
+                    resp.Close();
+                    Console.WriteLine("Processed POST request");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
