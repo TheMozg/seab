3bbe1907b6a402e0ea4e5c81571ca2737a9b09b8
TheMozg <fedor.md@gmail.com>
2015-04-18 15:51:38 +0300

    Multi user support with cookies

--- Server/cli.cs
+++ Server/cli.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Net;
 using Core;
 using System.IO;
@@ -11,7 +12,7 @@ namespace Server
 {
     class CLI
     {
-        AdressBook ab = new AdressBook();
+        ConcurrentDictionary<String, AdressBook> abs = new ConcurrentDictionary<string, AdressBook>();
         HttpListener listener = new HttpListener();
         public CLI()
         {
@@ -29,7 +30,7 @@ namespace Server
                 Console.WriteLine("New thread despatched");
             }
         }
-        void processPostOneRequest(HttpListenerContext context)
+        void processPostOneRequest(HttpListenerContext context, AdressBook ab)
         {
             Console.WriteLine("Processing POST request (create contact)");
 
@@ -41,8 +42,7 @@ namespace Server
                 contact.surname = parser.Parameters[Strings.KeyContactSnam];
                 contact.number = parser.Parameters[Strings.KeyContactNumb];
                 contact.mail = parser.Parameters[Strings.KeyContactMail];
-                lock (ab)
-                    ab.Add(contact);
+                ab.Add(contact);
             }
             context.Response.Close();
 
@@ -50,24 +50,39 @@ namespace Server
         }
         void processRequest(HttpListenerContext context)
         {
+            Cookie cookie = context.Request.Cookies["id"];
+            String id = "";
+            if(cookie != null)
+                id = cookie.Value;
+            Console.WriteLine("id is {0}", id);
+            if (String.IsNullOrEmpty(id))
+            {
+                Guid guid = Guid.NewGuid();
+                id = guid.ToString();
+                cookie = new Cookie("id", id);
+                cookie.Path = "/";
+            }
+            context.Response.SetCookie(cookie);
+            AdressBook ab = abs.GetOrAdd(id, new AdressBook());
+
             String rawUrl = context.Request.RawUrl;
             Console.WriteLine("Requested uri: {0}", rawUrl);
             if (rawUrl.StartsWith("/"))
                 rawUrl = rawUrl.Remove(0, 1);
             if (rawUrl == Strings.UriPostOne)
             {
-                processPostOneRequest(context);
+                processPostOneRequest(context, ab);
             }
             if (rawUrl == Strings.UriGetAll)
             {
-                processGetAllRequest(context);
+                processGetAllRequest(context, ab);
             }
             if (rawUrl == Strings.UriSearch)
             {
-                processSearchRequest(context);
+                processSearchRequest(context, ab);
             }
         }
-        private void processSearchRequest(HttpListenerContext context)
+        private void processSearchRequest(HttpListenerContext context, AdressBook ab)
         {
             Console.WriteLine("Processing POST request (search)");
 
@@ -80,23 +95,20 @@ namespace Server
                 searchBy = parser.Parameters[Strings.KeySearchType];
             }
             List<Contact> searchResults = new List<Contact>();
-            lock (ab)
+            switch (searchBy)
             {
-                switch (searchBy)
-                {
-                    case Strings.SearchTypeName:
-                        searchResults = ab.Search(searchString, SearchType.Name);
-                        break;
-                    case Strings.SearchTypeNplS:
-                        searchResults = ab.Search(searchString, SearchType.NameAndSurname);
-                        break;
-                    case Strings.SearchTypeNumb:
-                        searchResults = ab.Search(searchString, SearchType.Phone);
-                        break;
-                    case Strings.SearchTypeAll:
-                        searchResults = ab.Search(searchString, SearchType.All);
-                        break;
-                }
+                case Strings.SearchTypeName:
+                    searchResults = ab.Search(searchString, SearchType.Name);
+                    break;
+                case Strings.SearchTypeNplS:
+                    searchResults = ab.Search(searchString, SearchType.NameAndSurname);
+                    break;
+                case Strings.SearchTypeNumb:
+                    searchResults = ab.Search(searchString, SearchType.Phone);
+                    break;
+                case Strings.SearchTypeAll:
+                    searchResults = ab.Search(searchString, SearchType.All);
+                    break;
             }
 
             Misc.Serialize(context.Response.OutputStream, searchResults);
@@ -105,12 +117,11 @@ namespace Server
             Console.WriteLine("Processed request");
         }
 
-        private void processGetAllRequest(HttpListenerContext context)
+        private void processGetAllRequest(HttpListenerContext context, AdressBook ab)
         {
             Console.WriteLine("Processing GET request (getall)");
 
-            lock (ab)
-                Misc.Serialize(context.Response.OutputStream, ab.MasterList);
+            Misc.Serialize(context.Response.OutputStream, ab.MasterList);
             context.Response.Close();
 
             Console.WriteLine("Processed request");
